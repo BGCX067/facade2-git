c1712abf868431ab3d431accdd23a9ec59ce75ae
christian riekoff <info@texone.org>
2013-04-08 11:18:22 +0200

    added start bat

---
+++ app/start.bat
@@ -0,0 +1,2 @@
+ java -Xms256m -Xmx512m -classpath lib/c4.jar;lib/gluegen.jar;lib/gluegen-rt.jar;lib/jogl.all.jar;lib/jna.jar;lib/gstreamer-java.jar cc.creativecomputing.facade.view.app.C4EditorApp
+pause
